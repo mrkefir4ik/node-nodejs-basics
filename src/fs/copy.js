78d739cadabd0a7c0afd2f128dbd2197ac8f3af9
Daniil Aharodnikau <joker19980621@gmail.com>
2022-05-31 20:43:47 +0300

    feat: finished with copying

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,11 @@
 export const copy = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    if (!fs.existsSync('files') || fs.existsSync('files_copy')) throw new Error('FS operation failed');
+    else {
+        fs.mkdir('files_copy/files', { recursive: true }, (e) => null);
+        const filesToCopy = fs.readdirSync('files')
+        filesToCopy.forEach(e => {
+            fs.copyFile(`files/${e}`, `files_copy/files/${e}`, e => null)
+        })
+    }
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -2,7 +2,7 @@ export const create = async () => {
     const fs = await import('fs');
     if (fs.existsSync('files/fresh.txt')) throw new Error('FS operation failed');
     else {
-        fs.mkdirSync('files', { recursive: true });
-        fs.writeFileSync('files/fresh.txt', 'I am fresh and young', (e) => null);
+        fs.mkdir('files', { recursive: true }, (e) => null);
+        fs.writeFile('files/fresh.txt', 'I am fresh and young', (e) => null);
     }
 };
