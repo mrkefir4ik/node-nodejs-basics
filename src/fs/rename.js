71fc31a5b6782a04c75b7ca96a8ddd523be06969
Daniil Aharodnikau <joker19980621@gmail.com>
2022-05-31 21:10:12 +0300

    feat: finished with renaming

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,12 @@
 export const rename = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    const filesInFolder = fs.readdirSync('files')
+    if (filesInFolder.length !== 0 &&
+        filesInFolder.some(e => e == 'wrongFilename.txt') &&
+        filesInFolder.every(e => e !== 'properFilename.md')) {
+        fs.rename('files/wrongFilename.txt', 'files/properFilename.md', e => null);
+    }
+    else {
+        throw new Error('FS operation failed');
+    }
 };
