3b0df3a0a6ade12fefab5f81b3ce4457b79b3d86
Daniil Aharodnikau <joker19980621@gmail.com>
2022-05-31 19:40:08 +0300

    init: forked repo, branched and finished with first file on the list

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,8 @@
 export const create = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    if (fs.existsSync('files/fresh.txt')) throw new Error('FS operation failed');
+    else {
+        fs.mkdirSync('files', { recursive: true });
+        fs.writeFileSync('files/fresh.txt', 'I am fresh and young', (e) => null);
+    }
 };
