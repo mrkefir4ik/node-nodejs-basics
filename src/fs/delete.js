87ed7124b3acc21dbf8741c05ba6088313e5be1a
Daniil Aharodnikau <joker19980621@gmail.com>
2022-05-31 21:14:54 +0300

    feat: finished with deletion

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,11 @@
 export const remove = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    const filesInFolder = fs.readdirSync('files')
+    if (filesInFolder.length !== 0 &&
+        filesInFolder.some(e => e == 'fileToRemove.txt')) {
+        fs.unlink('files/fileToRemove.txt', () => null)
+    }
+    else {
+        throw new Error('FS operation failed');
+    }
 };
