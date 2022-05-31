6283189461c782a603d02eca428ff45ad5d6bbe0
Daniil Aharodnikau <joker19980621@gmail.com>
2022-05-31 21:23:25 +0300

    feat: finished with FS block

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,10 @@
 export const list = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    if (fs.existsSync('files')) {
+        const filesInFolder = fs.readdirSync('files')
+        console.log(filesInFolder);
+    }
+    else {
+        throw new Error('FS operation failed');
+    }
 };
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,13 @@
 export const read = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    if (fs.existsSync('files/fileToRead.txt')) {
+        fs.readFile('files/fileToRead.txt', 'utf8', (err, data) => {
+            if (!err) {
+                console.log(data);
+            }
+        });
+    }
+    else {
+        throw new Error('FS operation failed');
+    }
 };
