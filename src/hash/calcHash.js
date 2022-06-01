a2c7d1bfd74af5260930a0b54a173bbde6c499fa
Daniil Aharodnikau <joker19980621@gmail.com>
2022-06-01 21:46:53 +0300

    feat: finished with hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
 export const calculateHash = async () => {
-    // Write your code here 
+    const crypto = await import('crypto');
+    const fsPromises = await import('fs/promises');
+
+    const giveHash = async () => {
+        const result = await fsPromises.readFile('src/hash/files/fileToCalculateHashFor.txt')
+            .then(data => crypto.createHash('sha256').update(data).digest('hex'))
+
+        return result
+    }
+    console.log(await giveHash());
+    return await giveHash()
+
 };
