e59760ee1731409f2f7ce4d1017550ba906ad9c9
Daniil Aharodnikau <joker19980621@gmail.com>
2022-06-02 18:38:02 +0300

    feat: finished with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,7 @@
 export const read = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    let stream = fs.createReadStream('src/streams/files/fileToRead.txt');
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk.toString())
+    })
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,7 @@
 export const write = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    let writeStream = fs.createWriteStream('src/streams/files/fileToWrite.txt');
+    process.stdin.on('data', data => {
+        writeStream.write(data.toString())
+    });
 };
