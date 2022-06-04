e9dc9e7292cea18363a99ee8301c964a68006bc4
Daniil Aharodnikau <joker19980621@gmail.com>
2022-06-04 16:52:34 +0300

    feat: added zip and threads solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,25 @@
 export const performCalculations = async () => {
-    // Write your code here
-};
+    const { Worker } = await import('worker_threads')
+    const os = await import('os')
+    const results = os.cpus().map((e, index, arr) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./src/wt/worker.js', { workerData: { data: 10 + index } });
+            worker.on('message', result => {
+                resolve({
+                    status: 'resolved',
+                    data: result,
+                })
+            });
+            worker.on('error', error => {
+                resolve({
+                    status: 'error',
+                    data: null,
+                })
+            });
+        })
+    })
+    return (await Promise.allSettled(results)).map(e => e.value);
+
+
+}
+performCalculations().then(data => console.log(data))
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,9 @@
 export const compress = async () => {
-    // Write your code here 
+    const fs = await import('fs');
+    const zlib = await import('zlib');
+
+    const zipStream = zlib.createGzip();
+    const file = fs.createReadStream('src/zip/files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('src/zip/files/archive.gz');
+    file.pipe(zipStream).pipe(writeStream);
 };
